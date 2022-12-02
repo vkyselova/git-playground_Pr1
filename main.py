b924bde33f652a302023396b67ae6498232e928f
vkyselova <vkyselova@kse.org.ua>
2022-12-02 09:39:11 +0200

    added the "You've lost" message

--- main.py
+++ main.py
@@ -66,3 +66,5 @@ while not is_game_over():
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+else:
+    print(":'( :'( :'( You've lost :'( :'( :'(")
